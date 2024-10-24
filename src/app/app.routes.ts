ec9942f0e37df8fd1281608cd7017bf54dd6a471
Nina Urazova <ninuspinus@bk.ru>
2024-10-24 18:35:33 +0300

    добавила компонент project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Route } from '@angular/router';
-import { IntroComponent, RegistrationComponent } from './components';
+import { IntroComponent, RegistrationComponent, ProjectComponent } from './components';
 
 export const appRoutes: Route[] = [
   { path: 'auth', component: IntroComponent },
   { path: 'auth/register', component: RegistrationComponent },
+  { path: 'project/:projectId', component: ProjectComponent },
 ];
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,3 @@
 export * from './auth/registration/registration.component';
 export * from './auth/intro/intro.component';
+export * from './project/project.component'
